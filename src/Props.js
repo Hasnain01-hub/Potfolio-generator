fc1661fe4402f4a204dda4197506f0c2afe18b73
Hasnain01-hub <hasnainsayyed833@gmail.com>
2022-10-18 00:18:02 +0530

    initial commit

---
+++ src/Props.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import React from "react";
+function Props({setcounter,counter, refs}) {
+const design=()=>{
+refs.current.style.fontWeight="lighter";
+}
+ return (
+  <>
+<input placeholder="Enter msg" onChange={(e)=>setcounter(e.target.value)}/>
+<button onClick={design}>Lighter</button>
+</>
+  );
+}
+export default Props;
